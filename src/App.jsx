a47da5e8ce1295bcd2a091dcf756feab931a5c74
JubairAnjumRony <rony.ict.iu@gmail.com>
2024-11-05 21:10:59 +0600

    taiwind daisy ui and react-router installed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-6xl'>kfk heelloo</h1>
+      <button className='btn'>Daisy</button>
+    </div>
+  );
+};
+
+export default App;
